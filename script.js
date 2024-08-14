08670664e6fa6c6f7b09b4877b4b33b41343b45b
André Wantil <wantiltrabalhos@gmail.com>
2024-08-14 10:47:27 -0300

    desafio concluido

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let tasks = [];
+const button = document.getElementById('addTaskButton');
 
+function seeIt(result) {
+    document.getElementById('taskList').innerHTML = result;
+}
+
+function addTask() {
+    let inputValue = document.getElementById('taskInput').value;
+
+    tasks.push(inputValue)
+    console.log(tasks)
+
+    let result = ``
+    for (let i = 0; i < tasks.length; i++) {
+        result += "<li>" + tasks[i] + `<button type='button'>X</button>` + "</li>"
+    }
+
+    seeIt(result);
+
+    document.getElementById('taskInput').value = ''
+}
